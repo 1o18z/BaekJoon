e527551a1603e9f73d4fae9cd87c66f26e81951b
eunji <qkrdmswl1018@naver.com>
2023-02-01 21:59:30 +0900

    2525번 : BufferedReader, split()

--- src/Joon2525.java
+++ src/Joon2525.java
@@ -1,17 +1,16 @@
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Joon2525 {
-     public static void main(String[] args){
-            Scanner scan = new Scanner(System.in);
-            int A = scan.nextInt();
-            int B = scan.nextInt();
-            int C = scan.nextInt();
-            int D = 60 * A + B;
+     public static void main(String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] str = br.readLine().split(" ");
+            int A = Integer.parseInt(str[0]);
+            int B = Integer.parseInt(str[1]);
+            int C = Integer.parseInt(br.readLine());
+            int D = 60 * A + B;    // 분으로 변경
             D = D + C;
-            int H = (D/60) % 24;
-            int M = D % 60;
-            System.out.println(H + " " + M);
+            System.out.println(((D/60) % 24) + " " + (D % 60));
 
     }
 }
