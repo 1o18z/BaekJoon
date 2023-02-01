793b932c3f26c6ad9f0dde8d433188fbab38f447
eunji <qkrdmswl1018@naver.com>
2023-02-01 21:28:09 +0900

    2884번 : BufferedReader, split()

--- src/Joon2884.java
+++ src/Joon2884.java
@@ -1,26 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Joon2884 {
-        public static void main(String[] args){
-            Scanner scan = new Scanner(System.in);
-            int H = scan.nextInt();
-            int M = scan.nextInt();
-            if(M >= 45){
-                M = M - 45;
-                System.out.println(H + " " + M);
-            } else if(M < 45 && H > 0){
-                M = (60 + M) - 45;
-                H = H - 1;
-                System.out.println(H + " " + M);
-            } else if(M < 45 && H == 0){
-                M = (60 + M) - 45;
-                H = 23;
+        public static void main(String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] str = br.readLine().split(" ");
+            int H = Integer.parseInt(str[0]);
+            int M = Integer.parseInt(str[1]);
+
+            if(M < 45){
+                H--;
+                M = (M + 60) - 45;
+                if(H < 0){
+                    H = 23;
+                }
                 System.out.println(H + " " + M);
             } else{
-                System.out.println(H + " " + M);
+                System.out.println(H + " " + (M - 45));
             }
-
-
         }
     }
-}
+
