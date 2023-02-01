c7457f7e64604d1748227a3a8823fe23287175bd
eunji <qkrdmswl1018@naver.com>
2023-02-01 13:08:29 +0900

    2739번

---
+++ src/Joon2739.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Joon2739 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        for(int i = 1; i < 10; i++){
+            System.out.println(N + " * " + i + " = " + N*i);
+        }
+    }
+}
