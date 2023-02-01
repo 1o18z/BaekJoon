365cafda799da4e4103ad94d2cea716db7b0e8d2
eunji <qkrdmswl1018@naver.com>
2023-02-01 15:58:25 +0900

    15552번

---
+++ src/Joon15552.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Joon15552 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write((A+B) + "\n"); // \n 안써주면 이상하게 출력됨!!
+        }
+            bw.flush();
+            bw.close();
+        }
+    }
+
+
