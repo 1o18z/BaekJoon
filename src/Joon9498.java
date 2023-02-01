8ba3a9441028cfeb28254822a8546bdd9c3665c0
eunji <qkrdmswl1018@naver.com>
2023-02-01 20:07:23 +0900

    9498번 : 삼항연산자

--- src/Joon9498.java
+++ src/Joon9498.java
@@ -3,18 +3,7 @@ public class Joon9498 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int score = scan.nextInt();
-        String grade = "";
-        if(score > 89 && score <= 100){
-            grade = "A";
-        } else if(score > 79 && score <= 89){
-            grade = "B";
-        } else if(score > 69 && score <= 79) {
-            grade = "C";
-        } else if(score > 59 && score <= 69) {
-            grade = "D";
-        } else{
-            grade = "F";
-        }
-        System.out.println(grade);
+
+        System.out.println(score >= 90 ? "A" : (score >= 80) ? "B" : (score >= 70) ? "C" : (score >= 60) ? "D" : "E" );
     }
 }
