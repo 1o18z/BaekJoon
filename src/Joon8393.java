aead5177f1bcd2b17fc35ad695e883e7bb3fb378
eunji <qkrdmswl1018@naver.com>
2023-02-01 21:13:22 +0900

    8983번 : BufferedReader

--- src/Joon8393.java
+++ src/Joon8393.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
+import java.io.*;
 public class Joon8393 {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int sum = 0;
-        for(int i=0; i<=n; i++){
-            sum += i;
-        }
+        for(int i=0; i<=n; i++) sum += i;
         System.out.println(sum);
     }
 }
