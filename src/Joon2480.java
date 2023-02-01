c2621fb3f15e7e685fa5201dbc327cf733428c60
eunji <qkrdmswl1018@naver.com>
2023-02-01 22:03:49 +0900

    2480번 : BufferedReader, split()

--- src/Joon2480.java
+++ src/Joon2480.java
@@ -1,10 +1,12 @@
+import java.io.*;
 import java.util.Scanner;
 public class Joon2480 {
-        public static void main(String[] args){
-            Scanner scan = new Scanner(System.in);
-            int A = scan.nextInt();
-            int B = scan.nextInt();
-            int C = scan.nextInt();
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] str = br.readLine().split(" ");
+            int A = Integer.parseInt(str[0]);
+            int B = Integer.parseInt(str[1]);
+            int C = Integer.parseInt(str[2]);
             int money = 0;
             int max = A;
 
