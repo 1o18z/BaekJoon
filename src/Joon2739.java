9acf1fdfbf631c9539af13aa46c59fe67d080ffa
eunji <qkrdmswl1018@naver.com>
2023-02-01 21:41:42 +0900

    2739번 : BufferedReader

--- src/Joon2739.java
+++ src/Joon2739.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Joon2739 {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
         for(int i = 1; i < 10; i++){
             System.out.println(N + " * " + i + " = " + N*i);
         }
