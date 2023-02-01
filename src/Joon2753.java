0754ff12f7b6eeff89e95187b485131ed9093088
eunji <qkrdmswl1018@naver.com>
2023-02-01 21:38:34 +0900

    2753번 : BufferedReader

--- src/Joon2753.java
+++ src/Joon2753.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+import java.io.*;
 public class Joon2753 {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int year = scan.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
         System.out.println((year % 4 == 0) ? ((year % 400 == 0) ? 1 : year % 100 == 0 ? 0 : 1) : 0);
     }
 }
