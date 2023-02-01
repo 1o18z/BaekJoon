c50e9e2b165a8099385ad71579613dd4e4116b04
eunji <qkrdmswl1018@naver.com>
2023-02-01 22:19:40 +0900

    2439번 : BufferedReader

--- src/Joon2439.java
+++ src/Joon2439.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
+import java.io.*;
+
 public class Joon2439 {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         for(int i=N; i>=1; i--){
-            for(int j=1; j<=i; j++){
+            for(int j=1; j<i; j++){
                 System.out.print(" ");
             }
             for(int k=0; k<=N-i; k++){
