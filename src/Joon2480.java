facfde3e05a8ea4246a8edbb4a759fcbed7b4cc3
eunji <qkrdmswl1018@naver.com>
2023-02-01 22:07:00 +0900

    2480번 : BufferedReader, StringTokenizer

--- src/Joon2480.java
+++ src/Joon2480.java
@@ -1,30 +1,30 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Joon2480 {
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String[] str = br.readLine().split(" ");
-            int A = Integer.parseInt(str[0]);
-            int B = Integer.parseInt(str[1]);
-            int C = Integer.parseInt(str[2]);
-            int money = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
             int max = A;
 
             if(max < B) max = B;
             if(max < C) max = C;
 
             if(A == B && A == C){
-                money = 10000 + A*1000;
+                System.out.println(10000 + A*1000);
             } else if(A == B && A != C){
-                money = 1000 + A*100;
+                System.out.println(1000 + A*100);
             } else if(A == C && A != B){
-                money = 1000 + A*100;
+                System.out.println(1000 + A*100);
             } else if(B == C && B != A){
-                money = 1000 + B*100;
+                System.out.println(1000 + B*100);
             } else{
-                money = max*100;
+                System.out.println(max*100);
             }
-            System.out.println(money);
         }
     }
 
