47c89be3e9eb5ff20d25d532bab4ff23b97038f6
eunji <qkrdmswl1018@naver.com>
2023-02-01 21:07:05 +0900

    9498번 : BufferedReader

--- src/Joon9498.java
+++ src/Joon9498.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
-public class Joon9498 {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int score = scan.nextInt();
+import java.io.*;
 
+public class Joon9498 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
         System.out.println(score >= 90 ? "A" : (score >= 80) ? "B" : (score >= 70) ? "C" : (score >= 60) ? "D" : "E" );
     }
 }
