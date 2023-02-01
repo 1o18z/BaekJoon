2571041bd26e734ea4c33285696b59bb15077adb
eunji <qkrdmswl1018@naver.com>
2023-02-01 21:53:28 +0900

    25304번 : BufferedReader, split()

--- src/Joon25304.java
+++ src/Joon25304.java
@@ -1,24 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Joon25304 {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int X = scan.nextInt(); // 총액
-        int N = scan.nextInt(); // 물건 수
-        int a = 0;
-        int b = 0;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine()); // 총액
+        int N = Integer.parseInt(br.readLine()); // 물건 수
         int arr[] = new int[N];
         int total = 0;
 
         for(int i=0; i<N;i++){
-            a = scan.nextInt();
-            b = scan.nextInt();
+            String[] str = br.readLine().split(" ");
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
             arr[i] = a * b;
         }
         for(int j=0; j<arr.length; j++){
             total += arr[j];
         }
-        String result = (total == X) ? "Yes" : "No";
-        System.out.println(result);
+        System.out.println(total == X ? "Yes" : "No");
     }
 }
