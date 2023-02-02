9d6e33d88ba692f5eefe5437fdffc060ae3bfc0c
eunji <qkrdmswl1018@naver.com>
2023-02-02 18:00:47 +0900

    10950번 : 코드 정렬

--- src/Joon10950.java
+++ src/Joon10950.java
@@ -2,20 +2,20 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Joon10950 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int arr[] = new int[T];
-        for(int i=0; i<T; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-                int A = Integer.parseInt(st.nextToken());
-                int B = Integer.parseInt(st.nextToken());
-                arr[i] = A + B;
-            }
-        for(int j=0; j<arr.length; j++){
-            System.out.println(arr[j]);
-        }
-
-
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+    int arr[] = new int[T];
+    for (int i = 0; i < T; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int A = Integer.parseInt(st.nextToken());
+      int B = Integer.parseInt(st.nextToken());
+      arr[i] = A + B;
     }
+    for (int j = 0; j < arr.length; j++) {
+      System.out.println(arr[j]);
+    }
+
+
+  }
 }
