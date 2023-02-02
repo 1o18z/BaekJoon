fdece2bf504011c73f0328dde32b3b78943f25ca
eunji <qkrdmswl1018@naver.com>
2023-02-02 21:52:46 +0900

    1546번 : 코드 정리

--- src/Joon1546.java
+++ src/Joon1546.java
@@ -1,25 +1,21 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.util.Arrays;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Joon1546 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());  // 개수 입력받고
-    double[] arr = new double[N];
+    double[] arr = new double[Integer.parseInt(br.readLine())];
     double total = 0;
     StringTokenizer st = new StringTokenizer(br.readLine());
 
-    for (int i = 0; i < N; i++) {
-      arr[i] = Integer.parseInt(st.nextToken());  // 입력한 개수만큼 숫자 받고
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
-    Arrays.sort(arr);   // 오름차순으로 정렬 후
-    for(int i=0; i<N; i++){
-      total += (arr[i]/arr[arr.length-1])*100;  // arr값 / 최댓값 * 100한 거 총합을 total에 넣어서
+    Arrays.sort(arr);
+    for (int i = 0; i < arr.length; i++) {
+      total += (arr[i] / arr[arr.length - 1]) * 100;
     }
-    System.out.print(total/N);  // total을 N으로 나눠 평균 구함
+    System.out.print(total / arr.length);
   }
 }
