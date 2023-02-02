a7566ab90cec518d7278443d169610040081598a
eunji <qkrdmswl1018@naver.com>
2023-02-02 19:53:38 +0900

    10807번 : BufferedReader, StringTokenizer

--- src/Joon10807.java
+++ src/Joon10807.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Joon10807 {
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    int N = scan.nextInt();
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
     int[] arr = new int[N];
     int sum = 0;
 
+    StringTokenizer st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
-      arr[i] += scan.nextInt();
+      arr[i] += Integer.parseInt(st.nextToken());
     }
-    int count = scan.nextInt();
+    int count = Integer.parseInt(br.readLine());
     for (int j = 0; j < arr.length; j++) {
       if (arr[j] == count) sum++;
     }
