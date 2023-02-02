3199eacbd1f091ba497a2598718eb61740678b60
eunji <qkrdmswl1018@naver.com>
2023-02-02 18:03:42 +0900

    10952번 : import 정리

--- src/Joon10952.java
+++ src/Joon10952.java
@@ -1,5 +1,6 @@
-import java.io.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Joon10952 {
