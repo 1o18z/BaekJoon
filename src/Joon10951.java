2b30935aa913f6404e3204f9e34a2de73bcb221a
eunji <qkrdmswl1018@naver.com>
2023-02-03 21:47:31 +0900

    10951번 : hasNext()

--- src/Joon10951.java
+++ src/Joon10951.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Joon10951 {
+import java.util.*;
+
+public class Joon10951 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    while (scan.hasNext()) {
+      int A = scan.nextInt();
+      int B = scan.nextInt();
+      System.out.println(A + B);
+    }
+    scan.close();
+  }
 }
