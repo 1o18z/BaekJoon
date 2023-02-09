46f07cf297ddaa7d6c05735603e7f912930ff011
eunji <qkrdmswl1018@naver.com>
2023-02-09 23:04:50 +0900

    2751번 : Collections.sort

--- src/Joon2751.java
+++ src/Joon2751.java
@@ -1,21 +1,22 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Joon2751 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuilder sb = new StringBuilder();
-    boolean[] arr = new boolean[2000001];
-    // 수의 범위가 -1000000~1000000이므로 0의 기준을 인덱스 10000000로 잡음
 
     int N = Integer.parseInt(br.readLine());
+    ArrayList<Integer> list = new ArrayList<>();
 
     for (int i = 0; i < N; i++) {
-      arr[Integer.parseInt(br.readLine()) + 1000000] = true;
+      list.add(Integer.parseInt(br.readLine()));
     }
-    for(int i=0; i<arr.length; i++){
-      if(arr[i]){
-        sb.append((i-1000000)).append('\n');
-      }
+    Collections.sort(list);
+
+    for(int i : list){
+      sb.append(i).append('\n');
     }
     System.out.println(sb);
   }
