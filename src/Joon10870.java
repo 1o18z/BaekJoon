9ac9919f09fadedcc7616148cb74856526805af5
eunji <qkrdmswl1018@naver.com>
2023-02-26 13:22:20 +0900

    10870번

---
+++ src/Joon10870.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Joon10870 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int number = Integer.parseInt(br.readLine());
+    System.out.println(fibonacci(number));
+  }
+
+  static int fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
