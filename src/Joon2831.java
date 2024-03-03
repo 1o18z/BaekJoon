0e2b68ee13dda58fd28d0a67d1352807a967b232
박은지 <qkrdmswl1018@naver.com>
2024-03-03 11:36:42 +0900

    2831번

--- src/Joon2831.java
+++ src/Joon2831.java
@@ -1,32 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-  static int result = 0;
-  static List<Integer> boysA = new ArrayList<>(); // <0
-  static List<Integer> girlsA = new ArrayList<>();
-  
-  static List<Integer> boysB = new ArrayList<>(); // >0
-  static List<Integer> girlsB = new ArrayList<>();
+  static class GroupA {
+
+    PriorityQueue<Integer> girlsA; // <0
+    PriorityQueue<Integer> boysA; // >0
+
+    public GroupA() {
+      girlsA = new PriorityQueue<>();
+      boysA = new PriorityQueue<>();
+    }
+  }
+
+  static class GroupB {
+
+    PriorityQueue<Integer> girlsB; // >0
+    PriorityQueue<Integer> boysB; // <0
+
+    public GroupB() {
+      girlsB = new PriorityQueue<>();
+      boysB = new PriorityQueue<>();
+    }
+  }
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     int n = Integer.parseInt(br.readLine());
+    GroupA groupA = new GroupA();
+    GroupB groupB = new GroupB();
 
     StringTokenizer st = new StringTokenizer(br.readLine());
     for (int i = 0; i < n; i++) {
       int value = Integer.parseInt(st.nextToken());
       if (value < 0) {
-        boysA.add(-value);
+        groupB.boysB.add(Math.abs(value));
       } else {
-        boysB.add(value);
+        groupA.boysA.add(value);
       }
     }
 
@@ -34,40 +49,28 @@ public class Main {
     for (int i = 0; i < n; i++) {
       int value = Integer.parseInt(st.nextToken());
       if (value < 0) {
-        girlsB.add(-value);
+        groupA.girlsA.add(Math.abs(value));
       } else {
-        girlsA.add(value);
+        groupB.girlsB.add(value);
       }
     }
 
-    Collections.sort(girlsA);
-    Collections.sort(boysA);
-
-    int i = 0;
-    int j = 0;
-
-    while (i < boysA.size() && j < girlsA.size()) {
-      if (boysA.get(i) > girlsA.get(j)) {
+    int result = 0;
+    while (!groupB.boysB.isEmpty() && !groupB.girlsB.isEmpty()) {
+      int boy = groupB.boysB.poll();
+      int girl = groupB.girlsB.peek();
+      if (girl < boy) {
         result++;
-        i++;
-        j++;
-      } else {
-        i++;
+        groupB.girlsB.poll();
       }
     }
 
-    Collections.sort(girlsB);
-    Collections.sort(boysB);
-
-    i = 0;
-    j = 0;
-    while (i < girlsB.size() && j < boysB.size()) {
-      if (girlsB.get(i) > boysB.get(j)) {
+    while (!groupA.boysA.isEmpty() && !groupA.girlsA.isEmpty()) {
+      int girl = groupA.girlsA.poll();
+      int boy = groupA.boysA.peek();
+      if (boy < girl) {
         result++;
-        i++;
-        j++;
-      } else {
-        i++;
+        groupA.boysA.poll();
       }
     }
     System.out.println(result);
