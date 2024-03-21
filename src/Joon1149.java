f7e848dbb1b78b370c62e7b324d700f301a14825
박은지 <qkrdmswl1018@naver.com>
2024-03-21 18:40:17 +0900

    1149번

--- src/Joon1149.java
+++ src/Joon1149.java
@@ -1,19 +1,51 @@
-class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int n;
+  static int[][] board;
+  static int[][] priceBoard;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    int[][] dp = new int[n + 1][3];
 
-    for (int i = 1; i <= n; i++) {
+    n = Integer.parseInt(br.readLine());
+    board = new int[n][3];
+    priceBoard = new int[n][3];
+
+    for (int i = 0; i < n; i++) {
       StringTokenizer st = new StringTokenizer(br.readLine());
-      int a = Integer.parseInt(st.nextToken());
-      int b = Integer.parseInt(st.nextToken());
-      int c = Integer.parseInt(st.nextToken());
-      dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + a;
-      dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + b;
-      dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + c;
+      for (int j = 0; j < 3; j++) {
+        board[i][j] = Integer.parseInt(st.nextToken());
+      }
     }
-    System.out.println(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]));
+
+    priceBoard[0][0] = board[0][0];
+    priceBoard[0][1] = board[0][1];
+    priceBoard[0][2] = board[0][2];
+
+    int red = dp(n - 1, 0);
+    int green = dp(n - 1, 1);
+    int blue = dp(n - 1, 2);
+
+    System.out.println(Math.min(Math.min(red, green), blue));
+  }
+
+  private static int dp(int index, int color) {
+    if (priceBoard[index][color] == 0) {
+      switch (color) {
+        case 0:
+          priceBoard[index][0] = Math.min(dp(index - 1, 1), dp(index - 1, 2)) + board[index][0];
+        case 1:
+          priceBoard[index][1] = Math.min(dp(index - 1, 0), dp(index - 1, 2)) + board[index][1];
+        case 2:
+          priceBoard[index][2] = Math.min(dp(index - 1, 0), dp(index - 1, 1)) + board[index][2];
+      }
+    }
+
+    return priceBoard[index][color];
   }
 }
